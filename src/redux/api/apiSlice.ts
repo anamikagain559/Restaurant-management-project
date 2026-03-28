dc3a5e6abc52963eb4a84fcafbbda7d7628f5f83
anamikagain559 <anamika.zaimahtech@gmail.com>
2026-03-28 21:23:41 +0600

    added readme

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { axiosBaseQuery } from './axiosBaseQuery'
+import config from '../../config'
 
 // Define a service using a base URL and expected endpoints
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: axiosBaseQuery({ baseUrl: '/api/v1' }),
+    baseQuery: axiosBaseQuery({ baseUrl: config.baseUrl }),
     tagTypes: ['User', 'Menu', 'Order', 'Reservation', 'Table'],
     endpoints: () => ({}),
 })
